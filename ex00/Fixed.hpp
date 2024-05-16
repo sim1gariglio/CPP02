fcf2b3aff7463227a0c9af49c99bb6a807e7c15e
sim1gariglio <simone.gariglio1@gmail.com>
2024-05-16 11:26:18 +0200

    we

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: sgarigli <sgarigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 10:39:49 by sgarigli          #+#    #+#             */
-/*   Updated: 2024/05/16 10:40:41 by sgarigli         ###   ########.fr       */
+/*   Updated: 2024/05/16 11:19:22 by sgarigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,25 @@
 # define FIXED_HPP
 
 # include <iostream>
+# include <cmath>
 
 class Fixed
 {
 	public:
 		Fixed();
+		Fixed(const int value);
+		Fixed(const float value);
 		Fixed(const Fixed &src);
 		Fixed &operator=(const Fixed &rhs);
 		~Fixed();
 		int getRawBits(void) const;
 		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
 	private:
 		int _value;
 		static const int _bits = 8;
 };
 
+std::ostream& operator<<(std::ostream& o, Fixed const & rhs);
 #endif
