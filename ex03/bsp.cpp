8c8cdc443d5e4c27874f4db9e19b62561adc74cc
sim1gariglio <simone.gariglio1@gmail.com>
2024-09-11 11:48:16 +0200

    Update main.cpp in ex01 and bsp.cpp in ex03

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: sgarigli <sgarigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 10:19:46 by sgarigli          #+#    #+#             */
-/*   Updated: 2024/06/19 10:24:19 by sgarigli         ###   ########.fr       */
+/*   Updated: 2024/09/11 11:36:12 by sgarigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
 	Fixed const a1 = ((x - x1) * (y2 - y1)) - ((x2 - x1) * (y - y1));
 	Fixed const a2 = ((x - x2) * (y3 - y2)) - ((x3 - x2) * (y - y2));
 	Fixed const a3 = ((x - x3) * (y1 - y3)) - ((x1 - x3) * (y - y3));
+	if (a1 == 0 || a2 == 0|| a3 == 0)
+		return false;
 	if ((a1 >= 0 && a2 >= 0 && a3 >= 0) || (a1 <= 0 && a2 <= 0 && a3 <= 0))
 		return true;
 	return false;
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sgarigli <sgarigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 10:44:53 by sgarigli          #+#    #+#             */
-/*   Updated: 2024/06/19 11:24:56 by sgarigli         ###   ########.fr       */
+/*   Updated: 2024/09/11 11:35:24 by sgarigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int main(void)
 {
-	Point a(0.0, 0.0);
-	Point b(0.0, 1.0);
-	Point c(1.0, 0.0);
-	Point point(0.5, 0.5);
+	Point a(0, 0);
+	Point b(0, 5);
+	Point c(5, 0);
+	Point point(0, 1);
 
 	if (bsp(a, b, c, point))
 		std::cout << "The point is inside the triangle" << std::endl;
